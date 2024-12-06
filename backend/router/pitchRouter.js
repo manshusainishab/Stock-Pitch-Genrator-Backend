2ce85f0c93ad577aa79e45bba1b685418a2a8e31
manshusainishab <149303743+manshusainishab@users.noreply.github.com>
2024-12-06 18:07:17 +0530

    Delete backend directory

--- backend/router/pitchRouter.js
+++
@@ -1,7 +0,0 @@
-import express from 'express'
-import {getAllPitches, sendPitch } from '../controllers/pitchContoller.js';
-import { isUserAuthenticated } from '../middlewares/auth.js';
-const router = express.Router();
-router.post('/send', isUserAuthenticated,sendPitch)
-router.get('/getall', isUserAuthenticated, getAllPitches)
-export default router;
